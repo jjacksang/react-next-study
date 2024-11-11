580d121753c19b562632dd4aeeed64c282c62cd2
Sangwoo <jswoo1995@gmail.com>
2024-11-11 14:33:05 +0900

    useMemo 사용하기

--- todolist/src/main.jsx
+++ todolist/src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import B from "./exam/B.jsx";
 
-createRoot(document.getElementById("root")).render(
-    // <App />
-    <B />
-);
+createRoot(document.getElementById("root")).render(<App />);
