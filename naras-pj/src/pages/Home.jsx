7788c2dd2a134d2dd1284c066463f3f8718276e2
Sangwoo <jswoo1995@gmail.com>
2024-11-22 13:50:25 +0900

    api호출하기

--- naras-pj/src/pages/Home.jsx
+++ naras-pj/src/pages/Home.jsx
@@ -1,3 +1,18 @@
+import { useState, useEffect } from "react";
+
+import { fetchCountries } from "../api";
+
 export default function Home() {
+    const [countreis, setCountries] = useState([]);
+
+    const setInitData = async () => {
+        const data = await fetchCountries();
+        setCountries(data);
+    };
+
+    useEffect(() => {
+        setInitData();
+    }, []);
+
     return <div>Home</div>;
 }
